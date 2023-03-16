5a2ea992b6706d6a319126e4d6b7997343868145
ikolaus Fredy. M uropambin <123394261+ikouropmabin@users.noreply.github.com>
2023-03-16 18:05:09 +0000

    update file

---
+++ echo-print.php
@@ -0,0 +1,8 @@
+<?php
+	// Contoh echo
+	echo "<h1>Papua Tech Community</h1>";
+	echo "<p>Berbagi dan belajar bersama di komunitas.</p>";
+
+	//Contoh penggunaan print
+		print "<h1>Papua Coding School</h1>";
+		print "<p>belajar Coding dari dasar dengan mengikuti kursus.</p>";
